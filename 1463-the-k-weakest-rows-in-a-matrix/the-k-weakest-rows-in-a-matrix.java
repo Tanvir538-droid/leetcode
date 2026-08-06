964a4693261a3dd92244652f487f5f7bbde66871
M. Tanvir Rahman Ridhwan <tanvir252003@gmail.com>
2026-08-06 09:07:45 +0600

    Time: 2 ms (92.95%) | Memory: 46.5 MB (68.32%) - LeetSync

--- 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
+++ 1463-the-k-weakest-rows-in-a-matrix/the-k-weakest-rows-in-a-matrix.java
@@ -1,46 +1,52 @@
-class Solution {
-    public int[] kWeakestRows(int[][] mat, int k) {
-        //first count the number of 1 in each row
-        //save it into an arraylist
-        int[] rowSoldierCounter = new int[mat.length];
-        for(int i=0;i<mat.length;i++){
-            int count = 0;
-            for(int j=0;j<mat[0].length;j++){
-                if(mat[i][j]==1){
-                    count++;
-                }
-            }
-            rowSoldierCounter[i] = count;
-        }
+import java.util.PriorityQueue;
 
-        //create a treehmap <Integer,Integer> map
+class Solution {
+
+    public int[] kWeakestRows(int[][] mat, int k) {
+
         PriorityQueue<int[]> pq = new PriorityQueue<>(
-            //this is called the comparator 
-            (a,b) ->{
-                if(a[0]!=b[0]){
+            (a, b) -> {
+                if (a[0] != b[0]) {
                     return a[0] - b[0];
                 }
                 return a[1] - b[1];
             }
         );
 
-        //store the value according to their index
-        for(int i=0;i<rowSoldierCounter.length;i++){
-            pq.offer(new int[]{rowSoldierCounter[i],i});
+        // Count soldiers using binary search
+        for (int i = 0; i < mat.length; i++) {
+
+            int soldiers = countSoldiers(mat[i]);
+
+            pq.offer(new int[]{soldiers, i});
         }
 
-        //then based on the conditions I need to save k weakest rows 
-        //in other words their indexes into an array
         int[] ans = new int[k];
-        for(int i=0;i<k;i++){
-            ans[i] = pq.poll()[1];
-        } 
 
-        //now the ans array contains the k weakest rows
-        //return it
+        for (int i = 0; i < k; i++) {
+            ans[i] = pq.poll()[1];
+        }
 
         return ans;
+    }
 
+    // Returns the number of soldiers in one row
+    private int countSoldiers(int[] row) {
 
+        int left = 0;
+        int right = row.length;
+
+        while (left < right) {
+
+            int mid = left + (right - left) / 2;
+
+            if (row[mid] == 1) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+
+        return left;
     }
 }
